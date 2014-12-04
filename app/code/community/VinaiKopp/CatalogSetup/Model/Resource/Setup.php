4ffe3a9d39884bcc71406974a62f3370d7f0bd81
Rico Neitzel <rico@buro71a.de>
2014-12-04 01:51:32 +0000

    removed brackets

--- app/code/community/VinaiKopp/CatalogSetup/Model/Resource/Setup.php
+++ app/code/community/VinaiKopp/CatalogSetup/Model/Resource/Setup.php
@@ -271,7 +271,7 @@ class VinaiKopp_CatalogSetup_Model_Resource_Setup extends Mage_Catalog_Model_Res
         $storeLabels = $attribute['labels'];
         if (is_array($storeLabels)) {
             if (isset($attribute['attribute_id'])) {
-                $condition = array('attribute_id = (?)' => $attribute['attribute_id']);
+                $condition = array('attribute_id = ?' => $attribute['attribute_id']);
                 $this->getConnection()->delete($this->getTable('eav/attribute_label'), $condition);
 
                 $data = array();
